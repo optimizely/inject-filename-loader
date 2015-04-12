d277c5576ab723a5925752a2e786446bf5f0b65b
David Fox-Powell <dtothefp@gmail.com>
2015-04-12 12:28:57 -0700

    Add sourcemap support

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var loaderUtils = require('loader-utils');
 
-module.exports = function(source) {
+module.exports = function(source, map) {
   var options = loaderUtils.parseQuery(this.query);
 
   if (this.cacheable) {
     this.cacheable();
   }
 
-  return options.banner + options.inject + source + '\n\n' + options.footer;
-}
+  var wrapper = options.banner + options.inject + source + '\n\n' + options.footer;
+
+  this.callback(null, wrapper, map);
+};
